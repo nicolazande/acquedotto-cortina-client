d790ad21368394ce8e9d52c750201daf314a9a6c
nzandeg <itk.zandegiacomo@extaccount.com>
2024-07-03 16:08:52 +0200

    diobrasca

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -21,7 +21,8 @@ const UserList = () => {
                 const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/users`);
                 setUsers(response.data);
             } catch (error) {
-                alert(`Error fetching users: cmd = ${error.message} res = ${process.env.REACT_APP_API_URL}`);
+                alert(`Error fetching users: ${error.message}`);
+                alert(`request: ${process.env.REACT_APP_API_URL}/api/users`);
                 console.error('Error fetching users:', error); // Stampa l'errore completo nella console
             }
         };
