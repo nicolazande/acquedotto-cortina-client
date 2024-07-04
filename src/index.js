129485c426aaa438f5b7156d4f75af81f99c2c01
nzandeg <itk.zandegiacomo@extaccount.com>
2024-07-04 08:51:42 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import './index.css';
+import './styles/index.css';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
