94be8ff2b6c05e9347aebd9a94218f840cbe9c79
nzandeg <itk.zandegiacomo@extaccount.com>
2024-07-04 09:28:37 +0200

    kkk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Navbar.css'; // Importa il file CSS per lo stile della barra di navigazione
+import '../styles/Navbar.css';
 
-const Navbar = () => (
-    <nav className="navbar">
-        <ul className="navbar-nav">
-            <li className="nav-item">
-                <Link to="/" className="nav-link">Home</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/view-users" className="nav-link">Utenti</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/register" className="nav-link">Registrati</Link>
-            </li>
-        </ul>
-    </nav>
-);
+const Navbar = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    // Chiudi il dropdown quando si clicca fuori da esso
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setDropdownOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="dropdown" ref={dropdownRef}>
+                <button className="dropbtn" onClick={toggleDropdown}>
+                    <div className="menu-icon">&#9776;</div> {/* Unicode per l'icona delle barre */}
+                </button>
+                <div className={`dropdown-content ${dropdownOpen ? 'show' : ''}`}>
+                    <Link to="/" className="nav-link" onClick={() => setDropdownOpen(false)}>Home</Link>
+                    <Link to="/view-users" className="nav-link" onClick={() => setDropdownOpen(false)}>Utenti</Link>
+                    <Link to="/register" className="nav-link" onClick={() => setDropdownOpen(false)}>Registrati</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
 
 export default Navbar;
