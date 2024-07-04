1c69cdedc05f815c19b1a434019942fe9e1475d2
nzandeg <itk.zandegiacomo@extaccount.com>
2024-07-04 15:59:47 +0200

    gigi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,8 @@ import '../styles/Home.css'; // Assicurati di aver importato il file CSS corrett
 const Home = () => (
     <div className="home-container">
         <div className="home-content">
-            <h1>Acquedotto di Cortina</h1>
-            <p>Gestisci la tua acqua in modo efficiente</p>
+            <h1>Cooperativa di gestione acquedotto</h1>
+            <p>Vicina di Zuel di Sopra</p>
         </div>
         <div className="home-bottom-bar"></div>
     </div>
