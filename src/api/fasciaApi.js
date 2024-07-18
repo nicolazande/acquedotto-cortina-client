83ad1220e2c84a4569bbe366d3c7eefa04c0273f
nzandeg <itk.zandegiacomo@extaccount.com>
2024-07-18 18:22:20 +0200

    fasce

--- src/api/fasciaApi.js
+++ src/api/fasciaApi.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 
 const API_URL = `${process.env.REACT_APP_API_URL}/api/fasce`;
 
-const fasciaApi = {
+const fasciaApi =
+{
     createFascia: (data) => axios.post(API_URL, data),
     getFasce: () => axios.get(API_URL),
     getFascia: (id) => axios.get(`${API_URL}/${id}`),
     updateFascia: (id, data) => axios.put(`${API_URL}/${id}`, data),
-    deleteFascia: (id) => axios.delete(`${API_URL}/${id}`)
+    deleteFascia: (id) => axios.delete(`${API_URL}/${id}`),
+    associateListino: (fasciaId, listinoId) => axios.post(`${API_URL}/${fasciaId}/listini/${listinoId}`),
+    getListino: (id) => axios.get(`${API_URL}/${id}/listino`),
 };
 
 export default fasciaApi;
