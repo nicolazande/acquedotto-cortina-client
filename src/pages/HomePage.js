5efb4ede8087effd97529fcc1a1886e6e2ac211b
nzandeg <itk.zandegiacomo@extaccount.com>
2024-07-22 18:22:35 +0200

    ll

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,8 @@ import '../styles/HomePage.css';
 const HomePage = () => {
     return (
         <div className="homepage">
-            <h1>Benvenuti a Acquedotto Zuel</h1>
-            <p>Gestisci i tuoi clienti, contatori, edifici, letture, fatture, servizi, articoli, listini, fasce e scadenze in un unico posto.</p>
+            <h1>Acquedotto Zuel</h1>
+            <p>.</p>
         </div>
     );
 };
